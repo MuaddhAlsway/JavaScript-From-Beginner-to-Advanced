0d96e91f9a79708c277ac97f3484c2565e4b5615
Muaddh-Alswaiee <150309909+MuaddhAlsway@users.noreply.github.com>
2025-05-15 00:19:01 +0300

    Lesson 7 - Function

---
+++ Practice with Toturial.js
@@ -0,0 +1,17 @@
+/*
+function function1() {
+    console.log('hello')
+    console.log(2+2)
+}
+
+function1  ();
+function1();
+*/
+
+function calculateTax(cost, taxPercent) {
+    console.log(cost  * taxPercent);
+    
+}
+
+calculateTax(2000, 0.2);
+calculateTax(5000, 0.4);
