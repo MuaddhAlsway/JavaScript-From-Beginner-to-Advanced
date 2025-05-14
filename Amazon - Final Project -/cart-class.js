e3af420d75808cd8d93971c1cb809f7bb50d2203
Muaddh-Alswaiee <150309909+MuaddhAlsway@users.noreply.github.com>
2025-05-15 00:40:55 +0300

    Data - Final Project - Amazon Clone

---
+++ Amazon - Final Project -/cart-class.js
@@ -0,0 +1,91 @@
+class Cart {
+  
+  
+  cartItem;
+  #localSrorageKey; 
+  
+  constructor(localSrorageKey){
+    this.#localSrorageKey = localSrorageKey
+    this.#loadFromStorage();
+  }
+  #loadFromStorage() {
+    this.cartItem = JSON.parse(localStorage.getItem(this.#localSrorageKey));
+
+    if (!this.cartItem) {
+      this.cartItem = [{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2,
+        deliveryOptionId: '1'
+      }, {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1,
+        deliveryOptionId: '2'
+      }];
+    }
+  }
+
+  saveToStorage() {
+    localStorage.setItem(this.#localSrorageKey , JSON.stringify(this.cartItem));
+  }
+  
+  addToCart(productId) {
+    let matchingItem;
+  
+    this.cartItem.forEach((cartItem) => {
+      if (productId === cartItem.productId) {
+        matchingItem = cartItem;
+      }
+    });
+  
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      thiscartItem.push({
+        productId: productId,
+        quantity: 1,
+        deliveryOptionId: '1'
+      });
+    }
+  
+    this.saveToStorage();
+  }
+
+  removeFromCart(productId) {
+    const newCart = [];
+  
+    this.cartItem.forEach((cartItem) => {
+      if (cartItem.productId !== productId) {
+        newCart.push(cartItem);
+      }
+    });
+  
+    this.cartItem = newCart;
+  
+    this.saveToStorage();
+  }
+
+    
+  updateDeliveryOption(productId, deliveryOptionId) {
+    let matchingItem;
+  
+    thiscartItem.forEach((cartItem) => {
+      if (productId === cartItem.productId) {
+        matchingItem = cartItem;
+      }
+    });
+  
+    matchingItem.deliveryOptionId = deliveryOptionId;
+  
+    this.saveToStorage();
+  }   
+
+    };
+
+
+  const cart = new Cart('cart-oop');
+  const bussinesCart = new Cart('cart-business');
+
+
+
+  console.log(cart);
+  console.log(bussinesCart);
